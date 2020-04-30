4fa04194aa6a7a388c65df4df5b0785f69f68514
batu <ebatuhankaynak@gmail.com>
2020-04-30 18:48:00 +0300

    add boiler gan

--- project/res/srresnet/models.py
+++ project/res/srresnet/models.py
@@ -123,6 +123,102 @@ class SRResNet(nn.Module):
         sr_imgs = self.conv_block3(output)  # (N, 3, w * scaling factor, h * scaling factor)
 
         return sr_imgs
+    
+class Generator(nn.Module):
+    """
+    The generator in the SRGAN, as defined in the paper. Architecture identical to the SRResNet.
+    """
+
+    def __init__(self, large_kernel_size=9, small_kernel_size=3, n_channels=64, n_blocks=16, scaling_factor=4):
+        """
+        :param large_kernel_size: kernel size of the first and last convolutions which transform the inputs and outputs
+        :param small_kernel_size: kernel size of all convolutions in-between, i.e. those in the residual and subpixel convolutional blocks
+        :param n_channels: number of channels in-between, i.e. the input and output channels for the residual and subpixel convolutional blocks
+        :param n_blocks: number of residual blocks
+        :param scaling_factor: factor to scale input images by (along both dimensions) in the subpixel convolutional block
+        """
+        super(Generator, self).__init__()
+
+        # The generator is simply an SRResNet, as above
+        self.net = SRResNet(large_kernel_size=large_kernel_size, small_kernel_size=small_kernel_size,
+                            n_channels=n_channels, n_blocks=n_blocks, scaling_factor=scaling_factor)
+
+    def initialize_with_srresnet(self, srresnet_checkpoint):
+        """
+        Initialize with weights from a trained SRResNet.
+        :param srresnet_checkpoint: checkpoint filepath
+        """
+        srresnet = torch.load(srresnet_checkpoint)['model']
+        self.net.load_state_dict(srresnet.state_dict())
+
+        print("\nLoaded weights from pre-trained SRResNet.\n")
+
+    def forward(self, lr_imgs):
+        """
+        Forward prop.
+        :param lr_imgs: low-resolution input images, a tensor of size (N, 3, w, h)
+        :return: super-resolution output images, a tensor of size (N, 3, w * scaling factor, h * scaling factor)
+        """
+        sr_imgs = self.net(lr_imgs)  # (N, n_channels, w * scaling factor, h * scaling factor)
+
+        return sr_imgs
+    
+class Discriminator(nn.Module):
+    """
+    The discriminator in the SRGAN, as defined in the paper.
+    """
+
+    def __init__(self, kernel_size=3, n_channels=64, n_blocks=8, fc_size=1024):
+        """
+        :param kernel_size: kernel size in all convolutional blocks
+        :param n_channels: number of output channels in the first convolutional block, after which it is doubled in every 2nd block thereafter
+        :param n_blocks: number of convolutional blocks
+        :param fc_size: size of the first fully connected layer
+        """
+        super(Discriminator, self).__init__()
+
+        in_channels = 3
+
+        # A series of convolutional blocks
+        # The first, third, fifth (and so on) convolutional blocks increase the number of channels but retain image size
+        # The second, fourth, sixth (and so on) convolutional blocks retain the same number of channels but halve image size
+        # The first convolutional block is unique because it does not employ batch normalization
+        conv_blocks = list()
+        for i in range(n_blocks):
+            out_channels = (n_channels if i is 0 else in_channels * 2) if i % 2 is 0 else in_channels
+            conv_blocks.append(
+                ConvolutionalBlock(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                                   stride=1 if i % 2 is 0 else 2, batch_norm=i is not 0, activation='LeakyReLu'))
+            in_channels = out_channels
+        self.conv_blocks = nn.Sequential(*conv_blocks)
+
+        # An adaptive pool layer that resizes it to a standard size
+        # For the default input size of 96 and 8 convolutional blocks, this will have no effect
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((6, 6))
+
+        self.fc1 = nn.Linear(out_channels * 6 * 6, fc_size)
+
+        self.leaky_relu = nn.LeakyReLU(0.2)
+
+        self.fc2 = nn.Linear(1024, 1)
+
+        # Don't need a sigmoid layer because the sigmoid operation is performed by PyTorch's nn.BCEWithLogitsLoss()
+
+    def forward(self, imgs):
+        """
+        Forward propagation.
+        :param imgs: high-resolution or super-resolution images which must be classified as such, a tensor of size (N, 3, w * scaling factor, h * scaling factor)
+        :return: a score (logit) for whether it is a high-resolution image, a tensor of size (N)
+        """
+        batch_size = imgs.size(0)
+        output = self.conv_blocks(imgs)
+        output = self.adaptive_pool(output)
+        output = self.fc1(output.view(batch_size, -1))
+        output = self.leaky_relu(output)
+        logit = self.fc2(output)
+
+        return logit
+
 
 class TruncatedVGG19(nn.Module):
     """
